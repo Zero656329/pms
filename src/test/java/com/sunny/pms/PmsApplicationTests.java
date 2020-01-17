cdedd0ebbe20d856ee01923b0cc7ea96ed34cf93
韩文杰a <hanwja@fotile.com>
2020-01-17 09:50:29 +0800

    java 项目

---
+++ src/test/java/com/sunny/pms/PmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
